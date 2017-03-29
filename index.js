4234d88f34a6fe216be1cbc9bde017592fea0bca
Jon Borgonia <jon@gomagames.com>
2017-03-28 18:01:46 -1000

    more instructions

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 /**
  * Create a Class Trie
  * that can work with a trie data structure
+ *
+ * Instance Methods
+ *
+ * add
+ * @param word  string
+ * @returns void
+ *
+ * exists
+ * @param word  string
+ * @returns boolean
+ *          true if the word exists in the trie, else false
+ *
  */
 
